d7535073602203d26a5a922f5add9104659d9ac5
takgukim <ceman08071039@gmail.com>
2026-01-18 20:50:14 +0900

    풋터 수정

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -10,7 +10,7 @@
                 <div class="text-white-50 small">
                 <div>관리자 : 이태희</div>
                 <div>이메일 : ceman08071039@gmail.com</div>
-                <div>주소 : 경기도 안산시 단원구 시화호수로 633</div>
+                <div>주소 : 경기도 안산시 단원구 시화호수로 633 (반달섬)</div>
                 </div>
             </div>
             <div class="col-12 col-lg-5 text-center text-lg-end mt-2 mt-lg-0">
