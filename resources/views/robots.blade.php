2d14603431c3d2c3ed42709274715bb73e324535
takgukim <ceman08071039@gmail.com>
2026-03-04 07:01:27 +0900

    다음 웹마스터코드 다시 수정

--- resources/views/robots.blade.php
+++ resources/views/robots.blade.php
@@ -1,3 +1,6 @@
+# Daum 인증코드
+#DaumWebMasterTool:dff43d8ec53c25ef4f2ec80e4326d420e582f4303c21810d9df2858dd902ca44:rNqgoxJfVIy63czJ8LfgDg==
+
 User-agent: *
 Allow: /
 
@@ -13,8 +16,5 @@ Disallow: /password
 Disallow: /find-account
 Disallow: /find-password
 Disallow: /reset-password
-
-# Daum 인증코드
-#DaumWebMasterTool:dff43d8ec53c25ef4f2ec80e4326dd420e582f4303c21810d9df2858dd902ca44:rNqgoxJfVIy63czJ8LfgDg==
 
 Sitemap: {{ config('app.url') }}/sitemap.xml
