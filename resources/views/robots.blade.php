43b880432318516f195599f6f870ac1dccb71c01
takgukim <ceman08071039@gmail.com>
2026-03-04 06:57:27 +0900

    다음 웹마스터 등록

--- resources/views/robots.blade.php
+++ resources/views/robots.blade.php
@@ -14,4 +14,7 @@ Disallow: /find-account
 Disallow: /find-password
 Disallow: /reset-password
 
+# Daum 인증코드
+#DaumWebMasterTool:dff43d8ec53c25ef4f2ec80e4326dd420e582f4303c21810d9df2858dd902ca44:rNqgoxJfVIy63czJ8LfgDg==
+
 Sitemap: {{ config('app.url') }}/sitemap.xml
