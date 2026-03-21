bd95113ca085dbc04274a8ef7efa0d2bc7673e4a
takgukim <ceman08071039@gmail.com>
2026-03-21 18:19:15 +0900

    폴더 정리

--- app/Http/Controllers/TrafficController.php
+++ app/Http/Controllers/TrafficController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ConversionLogRequest;
+use App\Http\Requests\Traffics\ConversionLogRequest;
 use App\Services\TrafficAnalyticsService;
 
 class TrafficController extends Controller
--- app/Http/Requests/Traffics/ConversionLogRequest.php
+++ app/Http/Requests/Traffics/ConversionLogRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Traffics;
 
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
