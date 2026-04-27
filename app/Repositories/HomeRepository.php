6ef0f52bb1938ab730cb3fcebd2143f2f1227315
이태희 <ceman08071039@gmail.com>
2026-04-28 06:53:07 +0900

    홈 레퍼지토리 주석 추가

--- app/Repositories/HomeRepository.php
+++ app/Repositories/HomeRepository.php
@@ -5,10 +5,16 @@ namespace App\Repositories;
 use App\Models\Note;
 use Illuminate\Database\Eloquent\Collection;
 
+/**
+ * 홈 페이지 관련 데이터를 처리하는 리포지토리입니다.
+ */
 class HomeRepository
 {
     /**
-     * @return Collection<int, Note>
+     *  최신 블로그 글을 가져옵니다.
+     *
+     * @param integer $limit
+     * @return Collection
      */
     public function getLatestBlogs(int $limit = 5): Collection
     {
