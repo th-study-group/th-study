49d6454a5fd2a2c5384afd9883bd1420b74af08e
이태희 <ceman08071039@gmail.com>
2026-05-29 06:48:15 +0900

    타입추론 개선

--- app/Http/Middleware/CheckSessionVersion.php
+++ app/Http/Middleware/CheckSessionVersion.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * 강제 로그아웃 하기 위한 미들웨어
@@ -28,7 +29,7 @@ class CheckSessionVersion
             }
 
             if (session('session_version') != $user->session_version) {
-                auth()->logout();
+                Auth::logout();
 
                 session()->invalidate();
                 $request->session()->regenerateToken();
