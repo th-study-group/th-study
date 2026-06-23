ec1f402fcb8b5ec28d5c84959b3d975234a8f907
이태희 <ceman08071039@gmail.com>
2026-06-23 21:33:41 +0900

    포트폴리오 광고 위치 재변경

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -53,15 +53,15 @@
   <div class="container">
     <div class="row g-4">
       <div class="col-lg-4">
-        <div class="container text-center">
+        <h2 class="h2x mb-3">목차</h2>
+        <p class="leadx mb-0">표, 흐름도, 운영 문서, README 원문까지 포함</p>
+        <div class="container text-center mt-5">
           <x-adfit
               :unit="config('adfit.common.square.unit')"
               :width="config('adfit.common.square.width')"
               :height="config('adfit.common.square.height')" 
           />
         </div>
-        <h2 class="h2x mb-3">목차</h2>
-        <p class="leadx mb-0">표, 흐름도, 운영 문서, README 원문까지 포함</p>
       </div>
       <div class="col-lg-8">
         <div class="box pad toc">
