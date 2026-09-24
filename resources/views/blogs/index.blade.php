4dbf649ddc6179355fe6338003492afc1b85a0d3
이태희 <ceman08071039@gmail.com>
2026-09-24 10:24:11 +0900

    비로그인시 pwa  /blog   로딩 오류 수정

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -674,6 +674,15 @@
       };
 
       const startBlogPageNavigation = function() {
+        const isStandalonePwa = (typeof window.navigator.standalone !== 'undefined'
+          && window.navigator.standalone === true)
+          || !!(window.matchMedia
+            && window.matchMedia('(display-mode: standalone)').matches);
+
+        if (!isStandalonePwa) {
+          return;
+        }
+
         if (typeof window.showLoading === 'function') {
           window.showLoading();
         }
