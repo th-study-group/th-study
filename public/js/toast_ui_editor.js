bce5c5fb976315f6ba79de5082d45198fa36843e
이태희 <ceman08071039@gmail.com>
2026-09-25 19:54:12 +0900

    Revert "스크립트에서  태그 제거하는것으로 변경"
    This reverts commit 65bbbdaf2a6c31fdd3b2658b9135bef7dd011747.

--- public/js/toast_ui_editor.js
+++ public/js/toast_ui_editor.js
@@ -28,35 +28,6 @@ function looksLikeHtmlContent(value) {
     return /<\s*\/?\s*(p|br|strong|b|em|i|u|s|h[1-6]|ul|ol|li|blockquote|pre|code|a|div|span)\b/i.test(value);
 }
 
-function sanitizeToastEditorHtml(value) {
-    if (typeof value !== 'string' || value === '') {
-        return value || '';
-    }
-
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(value, 'text/html');
-    var adSelector = [
-        'iframe',
-        'script',
-        'ins.adsbygoogle',
-        '[data-ad-client]',
-        '[data-ad-slot]',
-        '[data-ad-format]',
-        '[data-ad-layout]',
-        '[id*="adsense" i]',
-        '[id*="adfit" i]',
-        '[class~="adsbygoogle"]',
-        '[class~="adsense"]',
-        '[class~="adfit"]'
-    ].join(',');
-
-    doc.body.querySelectorAll(adSelector).forEach(function (node) {
-        node.remove();
-    });
-
-    return doc.body.innerHTML;
-}
-
 window.initToastUiEditor = function (options) {
     var config = options || {};
     var Editor = window.toastui && window.toastui.Editor;
@@ -82,41 +53,14 @@ window.initToastUiEditor = function (options) {
         toolbarItems: config.toolbarItems || getToastEditorDefaultToolbarItems()
     });
 
-    var isSanitizing = false;
-
-    function syncEditorSource() {
-        if (!sourceEl || isSanitizing) {
-            return;
-        }
-
-        var currentHtml = editor.getHTML();
-        var sanitizedHtml = sanitizeToastEditorHtml(currentHtml);
-
-        if (sanitizedHtml !== currentHtml && typeof editor.setHTML === 'function') {
-            isSanitizing = true;
-            editor.setHTML(sanitizedHtml, false);
-            isSanitizing = false;
-        }
-
-        sourceEl.value = sanitizedHtml;
-    }
-
     if (hasInitialHtml && typeof editor.setHTML === 'function') {
-        editor.setHTML(sanitizeToastEditorHtml(initialValue), false);
+        editor.setHTML(initialValue, false);
     }
 
     if (sourceEl && syncOnChange) {
-        syncEditorSource();
+        sourceEl.value = editor.getHTML();
         editor.on('change', function () {
-            syncEditorSource();
-        });
-
-        var editorObserver = new MutationObserver(function () {
-            syncEditorSource();
-        });
-        editorObserver.observe(editorEl, {
-            childList: true,
-            subtree: true,
+            sourceEl.value = editor.getHTML();
         });
     }
 
